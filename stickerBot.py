81eb52a2fbee85f061b67974f2cab3d81d5ca26e
Vasileva <e.vasileva1@g.nsu.ru>
2021-09-30 13:48:49 +0700

    Read and send photo

--- stickerBot.py
+++ stickerBot.py
@@ -3,9 +3,16 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def welcome_start(message):
+    bot.send_message(message.chat.id, 'Отправьте мне своё фото')
+
+@bot.message_handler(content_types=['photo'])
+def sendMessage(message):
+        userPhoto = message.photo[2].file_id
+        bot.send_photo(message.chat.id, userPhoto)
+        bot.send_message(message.chat.id, 'Фото получено')
+
 
 #RUN
 bot.polling(none_stop=True)
