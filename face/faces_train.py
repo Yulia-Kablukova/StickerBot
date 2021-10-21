c58ad4fd5e5d97e622ccce1c1818900bff048dc6
Vasileva <e.vasileva1@g.nsu.ru>
2021-10-21 16:24:49 +0700

    recognize + collector

---
+++ face/faces_train.py
@@ -0,0 +1,49 @@
+import pickle
+
+import cv2
+import os
+import numpy as np
+from PIL import Image
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('C:\StickerBot\StickerBot\cascades\haarcascades\haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+directory = os.fsencode(image_dir)
+
+# for root, dirs, files in os.walk(image_dir):
+for file in os.listdir(directory):
+    # print("Я тутачки")
+    # for file in files:
+    # smth = file.decode('utf-8').endswith('.webp')
+    if file.decode('utf-8').endswith('.webp'):
+        path = os.path.join(image_dir.encode(), file)
+        # label = os.path.basename(root).replace(" ", "-").lower()
+        label = os.path.splitext(file.decode("utf-8"))[0]
+
+        if not label in label_ids:
+            label_ids[label] = current_id
+            current_id += 1
+        id_ = label_ids[label]
+
+        pil_image = Image.open(path).convert("L")  # grayscale
+        image_array = np.array(pil_image, "uint8")
+        faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            roi = image_array[y:y + h, x:x + w]
+            x_train.append(roi)
+            y_labels.append(id_)
+
+with open(r'C:\StickerBot\StickerBot\recognizers\pickles\face-labels.pickle', 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save(r'C:\StickerBot\StickerBot\recognizers\face-trainner.yml')
--- stickerBot.py
+++ stickerBot.py
@@ -1,18 +1,63 @@
+import os
+
 import telebot
 import config
+from PIL import Image
+
+from face.face import recognize
 
 bot = telebot.TeleBot(config.TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def welcome_start(message):
     bot.send_message(message.chat.id, 'Отправьте мне своё фото')
 
+
+i = 34
+
+
+@bot.message_handler(content_types=['sticker'])
+def send_message(message):
+    global i
+    user_photo_id = message.sticker.file_id
+    file_info = bot.get_file(user_photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open("sticker.webp", 'wb') as user_photo:
+        user_photo.write(downloaded_file)
+    user_photo.close()
+    user_photo = Image.open("sticker.webp")
+    user_photo.save(r'Y:\Programming\Python\StickerBot\set1\\' + str(i) + '.webp')
+    user_photo.close()
+    bot.send_message(message.chat.id, str(i))
+    i += 1
+
+
 @bot.message_handler(content_types=['photo'])
-def sendMessage(message):
-        userPhoto = message.photo[2].file_id
-        bot.send_photo(message.chat.id, userPhoto)
-        bot.send_message(message.chat.id, 'Фото получено')
+def send_message(message):
+    user_photo_id = message.photo[-1].file_id
+    file_info = bot.get_file(user_photo_id)
+
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open("image.png", 'wb') as user_photo:
+        user_photo.write(downloaded_file)
+    # ERROR - TeleBot: "A request to the Telegram API was unsuccessful. Error code: 400. Description: Bad Request:
+    # file is too big"
+    user_photo.close()
+    # TODO: name is random (like id), title from user
+    bot.send_message(message.chat.id, 'Фото получено')
+    id = recognize(os.getcwd() + '\image.png')
+    bot.send_message(message.chat.id, id)
+    # user_sticker_set_name = "awesome3_stickerpack_by_sticker_box_bot"
+    # user_photo = open("somepic.png", 'rb')
+    # bot.create_new_sticker_set(message.chat.id, user_sticker_set_name, "Awesome title", "😂", user_photo, None)
+    # user_photo.close()
+    # user_photo = open("thumb.png", 'rb')
+    # bot.set_sticker_set_thumb(user_sticker_set_name, message.chat.id, user_photo)
+    #user_photo.close()
+    #bot.send_message(message.chat.id, "Фото получено")
+    #bot.send_message(message.chat.id, "https://telegram.me/addstickers/" + user_sticker_set_name)
 
 
-#RUN
-bot.polling(none_stop=True)
+# RUN
+bot.polling(none_stop=True, timeout=100000)
